9371fa93dc373ca701e577343527f15f3d1a371f
Rkar-LogicUnion <appleidrkar@icloud.com>
2020-08-14 13:26:05 +0630

    Added New Post Component

--- src/components/ProfileBody.js
+++ src/components/ProfileBody.js
@@ -6,7 +6,7 @@ class ProfileBody extends Component {
       <div className="w-2/3 mx-auto">
         <div className=" grid grid-cols-5 gap-5">
           <div className="col-span-2">
-            <div className="menu-dark p-3 rounded">
+            <div className="card">
               <div className="flex justify-between items-center">
                 <h2 className="text-2xl text-gray-200 font-bold">Friends</h2>
                 <a
@@ -53,7 +53,32 @@ class ProfileBody extends Component {
               </div>
             </div>
           </div>
-          <div className="col-span-3 bg-green-500">World</div>
+          <div className="col-span-3">
+            <div className="card">
+              <div className="flex border-b border-gray-800 pb-3">
+                <img
+                  src="profile.jpg"
+                  className="w-16 h-16 rounded-full object-cover"
+                  alt=""
+                />
+                <input
+                  type="text"
+                  className=" flex-1 m-2 rounded-full bg-input px-3 text-white outline-none"
+                  placeholder="What's on your mind?"
+                />
+              </div>
+              <div className="flex justify-evenly mt-2">
+                <a href="#" className="btn-primary">
+                  <i class="fas fa-photo-video text-green-500 mr-2"></i>
+                  Photo/Video
+                </a>
+                <a href="#" className="btn-primary">
+                  <i class="fas fa-paper-plane text-blue-500 mr-2"></i>
+                  Post
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
